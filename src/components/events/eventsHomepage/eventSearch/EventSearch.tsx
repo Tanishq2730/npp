44896bd0bbc508c37f977b37846a4476feb3c84b
Tanishq2730 <66459762+Tanishq2730@users.noreply.github.com>
2025-06-02 17:48:51 +0530

    push

--- src/components/events/eventsHomepage/eventSearch/EventSearch.tsx
+++ src/components/events/eventsHomepage/eventSearch/EventSearch.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, TextInput, Title } from "@mantine/core";
+import { Button, Container, TextInput } from "@mantine/core";
 import { DatePickerInput } from "@mantine/dates";
 import { IconCalendar, IconFilterOff, IconSearch } from "@tabler/icons-react";
 import React from "react";
@@ -32,13 +32,7 @@ const EventSearch: React.FC<EventSearchProps> = ({
   return (
     <Container className={classes.container} size="xl" style={{marginRight:"0 !important"}}>
       <div className={classes.content}>
-        {/* <Title order={3} className={classes.subTitle}>
-          {titles.subTitle}
-        </Title>
-        <h2 className={classes.mainTitle}>{titles.mainTitle}</h2>
-        <div className={`subheading-5 ${classes.description}`}>
-          {titles.description}
-        </div> */}
+        
         <div className={classes.searchSection} >
           <div className={classes.inputContainer}>
             <IconSearch className={classes.icon} size="1rem" stroke={1.5} />
